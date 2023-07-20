0cfefc368197f463c6a47a96473ceaf9510241fb
Dean Smith <dean96@live.co.uk>
2023-07-20 19:56:49 +0100

    Created namespace

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+namespace MyNamespace{
+    class MyClass {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
